b9742a8983c7be59145d65f1d837cf10454eff20
Tony G <92600212+eangutierrez@users.noreply.github.com>
2023-10-29 12:57:27 -0500

    Add files via upload

---
+++ Case_Study_5_Data_Mart/2_Data_Exploration.sql
@@ -0,0 +1,268 @@
+/* --------------------
+   Case Study Questions
+   --------------------*/
+
+-- 1. What day of the week is used for each week_date value?
+/*
+Explanation: The clean_weekly_sales table has all the 
+information we need.  We can use the DAYNAME() function with 
+the DISTINCT keyword to find what day of the week is used
+for each week_date value.  The answer is Monday. 
+*/
+SELECT 
+	DISTINCT DAYNAME(week_date)
+FROM
+	clean_weekly_sales;
+
+
+-- 2. What range of week numbers are missing from the dataset?
+/*
+Explanation: The clean_weekly_sales table has all the 
+information we need.  The first step is to create a 
+recursive CTE that contains one column called value.
+After selecting 1 as the value, we use the UNION ALL
+keywords to stack another set on top of this 1.  The
+WHERE clause will limit to 52, which is the maximum 
+number of weeks in a year.
+
+After building our recursive CTE, we are ready to
+answer the question.  We will begin by selecting
+the value column from the recursive CTE, and use
+the WHERE clause to filter out all the values that
+are not in a subquery.  This subquery will find
+all the distinct week numbers from the 
+clean_weekly_sales table.
+*/
+WITH RECURSIVE seq AS 
+(
+	SELECT 
+		1 AS value
+	UNION ALL
+	SELECT
+		value + 1
+	FROM
+		seq
+	WHERE
+		value < 52
+)
+SELECT 
+	s.value AS missing_week_numbers
+FROM 
+	seq AS s
+WHERE 
+	value NOT IN
+	(
+		SELECT 
+			DISTINCT week_number
+		FROM 
+			clean_weekly_sales
+	);
+
+
+-- 3. How many total transactions were there for each year in the dataset?
+/*
+Explanation: The clean_weekly_sales table has all the 
+information we need.  After selecting our table and 
+grouping by the calendar_year, we can select the 
+calendar_year column and the sum of the transactions
+to find our answer.
+*/
+SELECT
+	calendar_year,
+	SUM(transactions)
+FROM
+	clean_weekly_sales
+GROUP BY
+	calendar_year;
+
+
+-- 4. What is the total sales for each region for each month?
+/*
+Explanation: The clean_weekly_sales table has all the 
+information we need.  After selecting our table and
+grouping by region and month_number, we can select
+the region, use the MONTHNAME() function to find the
+name of the month for each transaction, and the sum
+of sales to find our answer.
+*/
+SELECT
+	region,
+	MONTHNAME(week_date) AS month_name,
+	SUM(sales)
+FROM
+	clean_weekly_sales
+GROUP BY
+	region, month_number;
+
+
+-- 5. What is the total count of transactions for each platform?
+/*
+Explanation: The clean_weekly_sales table has all the 
+information we need.  After selecting our table and
+grouping by platform, we can select the platform and
+sum of transactions to find our answer.
+*/
+SELECT
+	platform,
+	SUM(transactions) AS number_of_transactions
+FROM
+	clean_weekly_sales
+GROUP BY
+	platform;
+
+
+-- 6. What is the percentage of sales for Retail vs Shopify for each month?
+/*
+Explanation: The clean_weekly_sales table has all the 
+information we need.  The first step is to create a 
+CTE called sales_cte, that selects the calendar_year,
+month_number, the month name, the platform, 
+selecting our table, and three CASE statements to
+find the total sales for Retail, Shopify, and the 
+sum of the sum of sales partitioned by calendar_year
+and month.  These CASE statements will make our final
+query a lot easier.  Note that we must group by the 
+first three columns to complete the CTE.
+
+After building our CTE, we are ready to answer the
+question.  The answer should include the calendar_year,
+the month, a column that finds the percentage of retail
+sales, and another column that calculates the percentage
+of shopify sales.
+*/
+WITH sales_cte AS
+(
+SELECT
+	calendar_year,
+	month_number,
+	MONTHNAME(week_date) AS month,
+	platform,
+	SUM(CASE
+			WHEN platform = 'Retail' THEN sales ELSE NULL
+		END) AS retail_sales,
+	SUM(CASE
+			WHEN platform = 'Shopify' THEN sales ELSE NULL
+		END) AS shopify_sales,
+	SUM(SUM(sales)) OVER(PARTITION BY calendar_year, month_number) AS monthly_total
+FROM
+	clean_weekly_sales
+GROUP BY
+	calendar_year, month_number, MONTHNAME(week_date)
+)
+SELECT
+	s.calendar_year,
+	s.month,
+	ROUND(s.retail_sales / s.monthly_total * 100, 2) AS retail_pct,
+	ROUND(s.shopify_sales / s.monthly_total * 100, 2) AS shopify_pct
+FROM
+	sales_cte AS s;
+
+
+-- 7. What is the percentage of sales by demographic for each year in the dataset?
+/*
+Explanation: The clean_weekly_sales table has all the 
+information we need.  We can use a process similar to
+the one from question 6.  The first step is to create a 
+CTE called yearly_sales_cte, that selects the calendar_year,
+demographic, and four CASE statements to
+find the total sales for couples, families, unknown, and
+the sum of the sum of sales partitioned by calendar_year  
+These CASE statements will make our final
+query a lot easier.  Note that we must group by the 
+calendar_year only to complete the CTE.  The combination of
+the CASE statements and grouping by the calendar_year alone
+creates a data table in wide format, that contains no
+NULL values.
+
+With our CTE completed, we can query our answer. The
+solution includes the calendar year, three columns that
+calculate the percentage of sales per demographic.    
+*/
+WITH yearly_sales_cte AS
+(
+	SELECT 
+		calendar_year,
+		demographic,
+		SUM(CASE
+				WHEN demographic = 'Couples' THEN sales ELSE NULL
+			END) AS sum_couples,
+		SUM(CASE
+				WHEN demographic = 'Families' THEN sales ELSE NULL
+			END) AS sum_families,
+		SUM(CASE
+				WHEN demographic = 'unknown' THEN sales ELSE NULL
+			END) AS sum_unknown,
+		SUM(SUM(sales)) OVER(PARTITION BY calendar_year) AS yearly_total
+	FROM
+		clean_weekly_sales
+	GROUP BY
+		calendar_year
+)
+SELECT
+	y.calendar_year,
+	ROUND(y.sum_couples / y.yearly_total * 100, 2) AS pct_couples,
+	ROUND(y.sum_families / y.yearly_total * 100, 2) AS pct_families,
+	ROUND(y.sum_unknown / y.yearly_total * 100, 2) AS pct_unknown
+FROM yearly_sales_cte AS y;
+
+
+-- 8. Which age_band and demographic values contribute the most to Retail sales?
+/*
+Explanation: The clean_weekly_sales table has all the 
+information we need.  After choosing our table, we need 
+to filter for all the 'Retail' records in the WHERE clause.
+After grouping by both age_band and demographic, we can 
+select the pertinent columns to answer the question.  The
+solution should include the age_band, demographic, the sum
+of sales, and a column that calculates the sum of sales 
+divided by the total sum of sales, multiplies by 100, and 
+rounds to two decimal places.  This calculation is the 
+percentage of contribution to sales.  
+*/ 
+SELECT 
+	age_band,
+	demographic, 
+	SUM(sales),
+	ROUND(100 * SUM(sales) / 
+		(
+			SELECT
+				SUM(sales)
+			FROM
+				clean_weekly_sales
+			WHERE
+				platform = 'Retail'
+		), 2) AS pct
+FROM
+	clean_weekly_sales
+WHERE
+	platform = 'Retail'
+GROUP BY 
+	age_band, demographic
+ORDER BY
+	4 DESC;
+
+
+-- 9.  Can we use the avg_transaction column to find the average transaction
+-- size for each year for Retail vs Shopify? If not - how would you calculate
+-- it instead?
+/*
+Explanation: The clean_weekly_sales table has all the 
+information we need.  After choosing our table and grouping 
+by calendar_year and platform, we are ready to finalize
+the query.  The answer should include the calendar_year, 
+platform, the average function on the avg_transaction column,
+and the sum of the sales divided by the sum of the 
+transactions.  The answer clearly shows that we can't use
+the avg_transaction column.  The average of averages is 
+generally not accurate.  A better practice is to divide 
+after finding the individual sums.
+*/
+SELECT
+	calendar_year,
+	platform,
+	ROUND(AVG(avg_transaction), 2) AS avg_using_column,
+	ROUND(SUM(sales) / SUM(transactions), 2) AS avg_calculation
+FROM
+	clean_weekly_sales
+GROUP BY
+	calendar_year, platform;
---
+++ Case_Study_5_Data_Mart/4_Bonus_Question.sql
@@ -0,0 +1,246 @@
+/* --------------------
+   Case Study Questions
+   --------------------*/
+
+/*
+Which areas of the business have the highest negative impact in sales metrics 
+performance in 2020 for the 12 week before and after period?
+
+* region
+* platform
+* age_band
+* demographic
+* customer_type
+
+Do you have any further recommendations for Danny’s team at Data Mart 
+or any interesting insights based off this analysis? 
+*/
+
+/*
+Explanation: The clean_weekly_sales table has all the 
+information we need.  We can adapt the last query we used
+on part 3 to answer each of these questions.  By simply
+updating the calendar_year to a different dimension, we
+can see how these changes affected each dimension.  Some
+of the highlights include:
+
+Region:
+* Asia was the most negatively affected area after
+implementing the change, with a 61,315,418 loss of 
+sales, which amounts to a -1.33% change.
+Platform:
+* Retail was the most negatively affected dimension
+after implementing the change, with a 117,464,107 
+loss of sales, which amounts to a -0.59% change.
+Age Band:
+* Unknown was the most negatively affected dimension
+after implementing the change, with a 44,645,418 loss
+of sales, which amounts to a -0.55% change.
+Demographic:
+* Unknown was the most negatively affected dimension
+after implementing the change, with a 44,645,418 loss
+of sales, which amounts to a -0.55% change.
+Customer Type:
+* Existing was the most negatively affected dimension
+after implementing the change, with a 51,510,403 loss
+of sales, which amounts to a -0.51% change.
+
+Based on our results, we can produce the following 
+recommendations:
+Region:
+* Although Asia had a negative reception to the changes,
+Africa, our region with the most sales, was more receptive.
+We should consider providing each region with the option to
+choose their package.
+Platform:
+* Shopify customers were more receptive to these changes.
+We should continue using the new packaging on this platform.
+Customer Type:
+* The negative change in existing and guest customers 
+was not made up by the positive sales changes brought by
+new customers.  If costs permit, we should allow for customers
+to choose what type of packaging they want.
+*/
+-- region:
+WITH week_selection_cte AS
+(
+	SELECT 
+		region,
+		week_number,
+		SUM(sales) AS total_sales
+	FROM
+		clean_weekly_sales
+	WHERE
+		week_number BETWEEN 13 AND 36
+	GROUP BY
+		region, week_number
+)
+, week_classifier_cte AS
+(
+	SELECT
+		ws.region,
+		SUM(CASE
+				WHEN ws.week_number BETWEEN 13 AND 24 THEN ws.total_sales ELSE NULL
+			END) AS sum_before_change,
+		SUM(CASE
+				WHEN ws.week_number BETWEEN 25 AND 36 THEN ws.total_sales ELSE NULL
+			END) AS sum_after_change
+	FROM
+		week_selection_cte AS ws
+	GROUP BY
+		ws.region
+)
+SELECT 
+	wc.region,
+	(wc.sum_after_change - wc.sum_before_change) AS change_in_sales,
+	ROUND((wc.sum_after_change - wc.sum_before_change) / wc.sum_before_change * 100, 2) AS pct_change
+FROM 
+	week_classifier_cte AS wc;
+	
+
+-- platform:
+WITH week_selection_cte AS
+(
+	SELECT 
+		platform,
+		week_number,
+		SUM(sales) AS total_sales
+	FROM
+		clean_weekly_sales
+	WHERE
+		week_number BETWEEN 13 AND 36
+	GROUP BY
+		platform, week_number
+)
+, week_classifier_cte AS
+(
+	SELECT
+		ws.platform,
+		SUM(CASE
+				WHEN ws.week_number BETWEEN 13 AND 24 THEN ws.total_sales ELSE NULL
+			END) AS sum_before_change,
+		SUM(CASE
+				WHEN ws.week_number BETWEEN 25 AND 36 THEN ws.total_sales ELSE NULL
+			END) AS sum_after_change
+	FROM
+		week_selection_cte AS ws
+	GROUP BY
+		ws.platform
+)
+SELECT 
+	wc.platform,
+	(wc.sum_after_change - wc.sum_before_change) AS change_in_sales,
+	ROUND((wc.sum_after_change - wc.sum_before_change) / wc.sum_before_change * 100, 2) AS pct_change
+FROM 
+	week_classifier_cte AS wc;
+	
+
+-- age_band:
+WITH week_selection_cte AS
+(
+	SELECT 
+		age_band,
+		week_number,
+		SUM(sales) AS total_sales
+	FROM
+		clean_weekly_sales
+	WHERE
+		week_number BETWEEN 13 AND 36
+	GROUP BY
+		age_band, week_number
+)
+, week_classifier_cte AS
+(
+	SELECT
+		ws.age_band,
+		SUM(CASE
+				WHEN ws.week_number BETWEEN 13 AND 24 THEN ws.total_sales ELSE NULL
+			END) AS sum_before_change,
+		SUM(CASE
+				WHEN ws.week_number BETWEEN 25 AND 36 THEN ws.total_sales ELSE NULL
+			END) AS sum_after_change
+	FROM
+		week_selection_cte AS ws
+	GROUP BY
+		ws.age_band
+)
+SELECT 
+	wc.age_band,
+	(wc.sum_after_change - wc.sum_before_change) AS change_in_sales,
+	ROUND((wc.sum_after_change - wc.sum_before_change) / wc.sum_before_change * 100, 2) AS pct_change
+FROM 
+	week_classifier_cte AS wc;
+	
+
+-- demographic: 
+WITH week_selection_cte AS
+(
+	SELECT 
+		demographic,
+		week_number,
+		SUM(sales) AS total_sales
+	FROM
+		clean_weekly_sales
+	WHERE
+		week_number BETWEEN 13 AND 36
+	GROUP BY
+		demographic, week_number
+)
+, week_classifier_cte AS
+(
+	SELECT
+		ws.demographic,
+		SUM(CASE
+				WHEN ws.week_number BETWEEN 13 AND 24 THEN ws.total_sales ELSE NULL
+			END) AS sum_before_change,
+		SUM(CASE
+				WHEN ws.week_number BETWEEN 25 AND 36 THEN ws.total_sales ELSE NULL
+			END) AS sum_after_change
+	FROM
+		week_selection_cte AS ws
+	GROUP BY
+		ws.demographic
+)
+SELECT 
+	wc.demographic,
+	(wc.sum_after_change - wc.sum_before_change) AS change_in_sales,
+	ROUND((wc.sum_after_change - wc.sum_before_change) / wc.sum_before_change * 100, 2) AS pct_change
+FROM 
+	week_classifier_cte AS wc;
+	
+
+-- customer_type: 
+WITH week_selection_cte AS
+(
+	SELECT 
+		customer_type,
+		week_number,
+		SUM(sales) AS total_sales
+	FROM
+		clean_weekly_sales
+	WHERE
+		week_number BETWEEN 13 AND 36
+	GROUP BY
+		customer_type, week_number
+)
+, week_classifier_cte AS
+(
+	SELECT
+		ws.customer_type,
+		SUM(CASE
+				WHEN ws.week_number BETWEEN 13 AND 24 THEN ws.total_sales ELSE NULL
+			END) AS sum_before_change,
+		SUM(CASE
+				WHEN ws.week_number BETWEEN 25 AND 36 THEN ws.total_sales ELSE NULL
+			END) AS sum_after_change
+	FROM
+		week_selection_cte AS ws
+	GROUP BY
+		ws.customer_type
+)
+SELECT 
+	wc.customer_type,
+	(wc.sum_after_change - wc.sum_before_change) AS change_in_sales,
+	ROUND((wc.sum_after_change - wc.sum_before_change) / wc.sum_before_change * 100, 2) AS pct_change
+FROM 
+	week_classifier_cte AS wc;
